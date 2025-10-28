ad5c3f6826a6ed51e90d2fac28bfbc3b2c8cf592
Lucas <lucas.115@hotmail.com>
2025-10-28 20:53:36 +0100

    Add a couple of utils

---
+++ src/main/java/com/lucas/utils/Utils.java
@@ -0,0 +1,56 @@
+package com.lucas.utils;
+
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+@SuppressWarnings("unused")
+public final class Utils {
+
+    public static final String EMPTY_STRING = "";
+    public static final Runnable NOOP = () -> {
+    };
+
+    private Utils() {
+    }
+
+    /**
+     * Sleeps for the specified number of milliseconds.
+     * If interrupted, invokes {@code onInterrupt} with the {@link InterruptedException}
+     *
+     * @param millis      millis
+     * @param onInterrupt on interrupt callback
+     */
+    public static void sleep(long millis, @Nonnull Consumer<? super InterruptedException> onInterrupt) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            onInterrupt.accept(e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    /**
+     * Returns the value from {@code getter} if non-null;
+     * otherwise obtains a new value from {@code supplier}, sets it using {@code setter}, and returns it.
+     *
+     * @param getter   getter
+     * @param setter   setter
+     * @param supplier supplier
+     * @param <T>      value type
+     * @return existing value from getter if non-null; otherwise the value produced by supplier
+     */
+    public static <T> @Nullable T computeIfAbsent(@Nonnull Supplier<T> getter,
+                                                  @Nonnull Consumer<T> setter,
+                                                  @Nonnull Supplier<T> supplier) {
+        T current = getter.get();
+        if (null != current) {
+            return current;
+        }
+        T next = supplier.get();
+        setter.accept(next);
+        return next;
+    }
+}
